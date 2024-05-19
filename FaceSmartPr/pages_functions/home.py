dcf04ee5392ef719b166268252f5e8b138a44534
Ameziane Douaa <128991644+douaaea@users.noreply.github.com>
2024-05-19 14:15:15 +0100

    Delete FaceSmartPr directory

--- FaceSmartPr/pages_functions/home.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-from ui.general import Ui_home
-
-class Home(QWidget):
-    def __init__(self):
-        super(Home, self).__init__()
-        self.ui=Ui_home()
-        self.ui.setupUi(self)
-
